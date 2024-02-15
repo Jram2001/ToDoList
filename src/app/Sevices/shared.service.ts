938180c32637760ff27ffe2ec79e1332e3334920
Jram2001 <jayaramasankaramoorthi@gmail.com>
2024-02-15 22:53:44 +0530

    Solved Null injector error when using http client in standalone components

--- src/app/Sevices/shared.service.ts
+++ src/app/Sevices/shared.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SharedService {
-  emittedValue: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
-  constructor() { }
-  emitValue(Event:any){
-        this.emittedValue.next(Event)
 
+export class SharedService {
+    constructor( private http : HttpClient ) { }
+
+  emittedValue: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  emitValue(Event:any){
+      this.emittedValue.next(Event)
+  }
+  GetBackendData():any{
+    this.http.get("http://localhost:4200/todo");
   }
 }
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = () => bootstrapApplication(AppComponent, {providers: [importProvidersFrom(HttpClientModule),importProvidersFrom(BrowserAnimationsModule)]});
 
 export default bootstrap;
+
