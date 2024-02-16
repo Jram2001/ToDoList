afa98f93a13f2d2938a04ce5ed2aeb7f0ed9bcab
CEN-036-23\Centizen_User <jayaram@centizen.com>
2024-02-16 17:51:32 +0530

    Made Http working and resolved null injector error

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,10 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-const bootstrap = () => bootstrapApplication(AppComponent, {providers: [importProvidersFrom(HttpClientModule),importProvidersFrom(BrowserAnimationsModule)]});
+const bootstrap = () => bootstrapApplication(AppComponent, { providers: [provideHttpClient(withFetch()), importProvidersFrom(NoopAnimationsModule)] });
 
 export default bootstrap;
 
