e86e08709685cd216879e087e76ed5b9150af532
Jram2001 <jayaramasankaramoorthi@gmail.com>
2024-02-19 22:29:21 +0530

    Got data from backend and displayed on frontend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,12 +12,30 @@ export class HomeComponent {
   emittedValue:boolean = false 
   constructor(private SharedService:SharedService){}
   tasks:any = [];
+  Todaysdate = new Date();
+  taskDate! :Date;
   ngOnInit(){
-    this.SharedService.TaskDta.subscribe( data => {this.tasks = data})
-   
+    this.SharedService.TaskDta.subscribe( data => {this.tasks = data;})
   }
   emit(Event:any){
     this.SharedService.emitValue(Event)
+  }
+  myColor(){
+    this.taskDate = new Date(this.tasks[0].CreatedOn);
+    let DiffrenceInMilliseconds = this.Todaysdate.getTime() - this.taskDate.getTime();
+    let DiffrenceInDays = this.Todaysdate.getDate() - this.taskDate.getDate();
+    if(DiffrenceInDays < 1 || this.tasks[0].Repetable != 0){
+        if(DiffrenceInMilliseconds / (1000 * 60 * 60 ) < 24){
+        return '#3ffc72'
+        }
+        else{
+          return '#ff2929'
+        }
+    }
+    else{
+      console.log(Math.floor(DiffrenceInMilliseconds / (1000 * 60 * 60 * 24)),DiffrenceInMilliseconds / (1000 * 60 * 60 ) < 24)
+      return '#ff2929'
+    }
   }
 }
 
