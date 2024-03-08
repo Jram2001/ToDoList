55eaf48ba79956b45f91f77564a05c90c63edab7
Jram2001 <jayaramasankaramoorthi@gmail.com>
2024-03-08 21:37:00 +0530

    Implimented routing in angular standalone component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,14 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
+import { SigninComponent } from './signin/signin.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideHttpClient(withFetch()) ]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideHttpClient(withFetch()),   
+     provideRouter([
+      { path: '', redirectTo: '/login', pathMatch:'full' },
+      { path: '**', redirectTo: '/login', pathMatch:'full' },
+      {path : 'home' , loadChildren : () => import('./home.route').then(r => r.routes)},
+      { path : 'login',component : SigninComponent }
+    ])]
 };
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppComponent } from './app/app.component';
 import { HomeComponent } from './app/home/home.component';
 import { DatePipe } from '@angular/common';
 import { importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { SigninComponent } from './app/signin/signin.component';
 const mergedConfig = {}
 bootstrapApplication(AppComponent, appConfig )
   .catch((err) => console.error(err));
